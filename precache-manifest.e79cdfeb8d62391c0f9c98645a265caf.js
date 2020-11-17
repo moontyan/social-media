71beae01f7458feb212bc4b9ba41910d2f7f81e0
moontyan <nikitosrazer@gmail.com>
2020-11-17 19:51:55 +0200

    Updates

---
+++ precache-manifest.e79cdfeb8d62391c0f9c98645a265caf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df70dfdda1164359ab05a59faeba1f91",
+    "url": "/social-media/index.html"
+  },
+  {
+    "revision": "8b9e9833fa6e32fbceb4",
+    "url": "/social-media/static/css/main.6bf76238.chunk.css"
+  },
+  {
+    "revision": "8cea8ee4acec00135a97",
+    "url": "/social-media/static/js/2.9286c0c4.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/social-media/static/js/2.9286c0c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b9e9833fa6e32fbceb4",
+    "url": "/social-media/static/js/main.74b36ebf.chunk.js"
+  },
+  {
+    "revision": "c0e4802e630fbc68bf6a",
+    "url": "/social-media/static/js/runtime-main.3ae4019e.js"
+  },
+  {
+    "revision": "48d270907fbb64b142ed88fc79e242b4",
+    "url": "/social-media/static/media/preloader.48d27090.svg"
+  },
+  {
+    "revision": "7cc7a630624d20f7797cb4c8e93c09c1",
+    "url": "/social-media/static/media/user.7cc7a630.png"
+  }
+]);
